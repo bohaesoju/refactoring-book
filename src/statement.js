104af10a06d080bf800dec01dd45a18eeeed2dc6
kimuyjoong <fr_bohaesoju@woowahan.com>
2020-04-21 10:58:09 +0900

    play 변수 분기

--- src/statement.js
+++ src/statement.js
@@ -29,6 +29,9 @@ let  invoicesJson =
 ];     
 
 function statement(invoice, plays){
+    const playFor = (aPerformance) => {
+        return plays[aPerformance.playID];
+    }
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice[0].customer})\n`;
@@ -37,7 +40,7 @@ function statement(invoice, plays){
     minimumFractionDigits: 2}).format;
 
     for(let perf of invoice[0].performances){
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         let thisAmount = amountFor(perf, play);
 
         //포인트를 적립한다.
@@ -55,21 +58,21 @@ function statement(invoice, plays){
     return result;
 }
 
-const amountFor = (perf, play) => {  //값이 바뀌지 않는 변수는 매개변수로 전달
+const amountFor = (aPerformance, play) => {  //값이 바뀌지 않는 변수는 매개변수로 전달
     let result = 0;   // 명확한 이름으로 변경
     switch(play.type){
         case "tragedy": 
             result = 4000;
-            if(perf.audience > 30){
-                result += 1000 * (perf.audience - 30);
+            if(aPerformance.audience > 30){
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":
             result = 30000;
-            if(perf.audience > 20){
-                result += 10000 + 500 * (perf.audience - 20);
+            if(aPerformance.audience > 20){
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }    
-            result += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
             throw new Error(`알 수 없는 장르: ${play.type}`)    
