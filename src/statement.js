481aab35c7299f64c9e1265be3a23a9e49cb2147
kimuyjoong <fr_bohaesoju@woowahan.com>
2020-04-21 16:54:00 +0900

    aperformance.play

--- src/statement.js
+++ src/statement.js
@@ -49,7 +49,7 @@ const renderPlainText = (data, plays) => {
 
     const amountFor = (aPerformance, play) => {  //값이 바뀌지 않는 변수는 매개변수로 전달
         let result = 0;   // 명확한 이름으로 변경
-        switch(playFor(aPerformance).type){
+        switch(aPerformance.play.type){
             case "tragedy": 
                 result = 4000;
                 if(aPerformance.audience > 30){
@@ -64,7 +64,7 @@ const renderPlainText = (data, plays) => {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)    
+                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)    
         }
         return result;  // 함수 안에서 값이 바뀌는 변수 반환
     }
@@ -73,7 +73,7 @@ const renderPlainText = (data, plays) => {
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
         //희극 관객 5명 마다 추가 포인트를 제공한다.
-        if("comedy" === playFor(aPerformance).type) 
+        if("comedy" === aPerformance.play.type) 
         result += Math.floor(aPerformance.audience / 5);
         return result;    
     }
@@ -103,7 +103,7 @@ const renderPlainText = (data, plays) => {
 
     for(let perf of data.performances){
         //청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     }
 
     result += `총액: ${usd(totalAmount())}\n`
