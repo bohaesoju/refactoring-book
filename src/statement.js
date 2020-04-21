c504f30fc5a2ec46cde7a84fbceb55e389b546b1
kimuyjoong <fr_bohaesoju@woowahan.com>
2020-04-21 09:28:59 +0900

    switch 문 중첩 함수로 변경

--- src/statement.js
+++ src/statement.js
@@ -38,25 +38,7 @@ function statement(invoice, plays){
 
     for(let perf of invoice[0].performances){
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch(play.type){
-            case "tragedy": 
-                thisAmount = 4000;
-                if(perf.audience > 30){
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if(perf.audience > 20){
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }    
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${play.type}`)    
-        }
+        let thisAmount = amountFor(perf, play);
 
         //포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -71,6 +53,28 @@ function statement(invoice, plays){
     result += `총액: ${format(totalAmount / 100)}\n`
     result +=  `적립 포인트: ${volumeCredits}점\n`;
     return result;
+}
+
+const amountFor = (perf, play) => {  //값이 바뀌지 않는 변수는 매개변수로 전달
+    let thisAmount = 0;   // 변수를 초기화하는 코드
+    switch(play.type){
+        case "tragedy": 
+            thisAmount = 4000;
+            if(perf.audience > 30){
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if(perf.audience > 20){
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }    
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`알 수 없는 장르: ${play.type}`)    
+    }
+    return thisAmount;  // 함수 안에서 값이 바뀌는 변수 반환
 }
 
 statement(invoicesJson, playsJson);
