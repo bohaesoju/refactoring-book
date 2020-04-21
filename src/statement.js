6fb7a670e2aeb8c03aeb9ad210358480c1f837b5
kimuyjoong <fr_bohaesoju@woowahan.com>
2020-04-21 15:49:28 +0900

    usd 함수 생성

--- src/statement.js
+++ src/statement.js
@@ -58,28 +58,31 @@ function statement(invoice, plays){
 
     const volumeCreditsFor = (aPerformance) => {
         let result = 0;
-        result += Math.max(perf.audience - 30, 0);
+        result += Math.max(aPerformance.audience - 30, 0);
         //희극 관객 5명 마다 추가 포인트를 제공한다.
-        if("comedy" === playFor(perf).type) 
-        result += Math.floor(perf.audience / 5);
+        if("comedy" === playFor(aPerformance).type) 
+        result += Math.floor(aPerformance.audience / 5);
         return result;    
+    }
+
+    const usd = (aNumber) => {
+        return new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD',
+            minimumFractionDigits: 2}).format(aNumber/100);
     }
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice[0].customer})\n`;
-    const format = new Intl.NumberFormat('en-US',
-        { style: 'currency', currency: 'USD',
-    minimumFractionDigits: 2}).format;
 
     for(let perf of invoice[0].performances){
         volumeCredits += volumeCreditsFor(perf);
         let thisAmount = amountFor(perf);
 
         //청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `총액: ${format(totalAmount / 100)}\n`
+    result += `총액: ${usd(totalAmount)}\n`
     result +=  `적립 포인트: ${volumeCredits}점\n`;
     return result;
 }
