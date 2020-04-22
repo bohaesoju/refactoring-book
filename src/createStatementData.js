166968effebe4497c9b97c42c7368ee690e63ba4
kimuyjoong <fr_bohaesoju@woowahan.com>
2020-04-22 16:52:39 +0900

    amountFor 를 performance 클래스로 변경

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -31,25 +31,7 @@ export default function createStatementData(invoice, plays) {
         return plays[aPerformance.playID];
     }
     const amountFor = (aPerformance, play) => {  //값이 바뀌지 않는 변수는 매개변수로 전달
-        let result = 0;   // 명확한 이름으로 변경
-        switch(aPerformance.play.type){
-            case "tragedy": 
-                result = 4000;
-                if(aPerformance.audience > 30){
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if(aPerformance.audience > 20){
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }    
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)    
-        }
-        return result;  // 함수 안에서 값이 바뀌는 변수 반환
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;  // 원본 함수인 amountFor() 도 계산기를 이용하도록 수정
     }
 
     const volumeCreditsFor = (aPerformance) => {
